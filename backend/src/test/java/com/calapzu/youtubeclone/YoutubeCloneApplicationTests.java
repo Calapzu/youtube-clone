0647961f8a198cc8c28171f99688f41d8a35b900
Calapzu <yamsoncalapzu@gmail.com>
2023-06-21 14:30:37 -0500

    Se crea la estructura basica del proyecto tanto como los moduos del backend como la del frontend

---
+++ backend/src/test/java/com/calapzu/youtubeclone/YoutubeCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calapzu.youtubeclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutubeCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
